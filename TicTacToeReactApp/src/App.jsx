9b05bf457dc681de6422588cb634ee03928db6e4
Tyson Potter <tysonpottersd@gmail.com>
2024-07-10 15:19:06 -0600

    updated local storage  fucntionality

--- TicTacToeReactApp/src/App.jsx
+++ TicTacToeReactApp/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Lobby from './Lobby'; // Ensure the correct import path
 import Grid from './Grid'
-// Function to fetch data from the API
 
-// Set the `inGame` variable to false
+
 localStorage.setItem('inGame', JSON.stringify(false));
+localStorage.setItem('gameId', JSON.stringify(null));
+localStorage.setItem('player', JSON.stringify(null));//x or o
+localStorage.setItem('playerName', JSON.stringify(null));
 const inGame = JSON.parse(localStorage.getItem('inGame'));
+
 function App() {
   return (
     <>
--- TicTacToeReactApp/src/Lobby.jsx
+++ TicTacToeReactApp/src/Lobby.jsx
@@ -1,41 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-async function fetchData() {
-  try {
-    const response = await fetch('http://localhost:3001/data');
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return []; // Return an empty array in case of error
-  }
-}
+
 
 function Lobby() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    async function getGames() {
-      const gamesData = await fetchData();
-      setGames(gamesData);
-    }
-
-    getGames();
-  }, []); // Empty dependency array means this useEffect runs once on mount
-
+  
   return (
     <>
       <button id="createGame">Create Game</button>
       <div id="games">Games place holder</div>
-      <ul>
-        {games.map((game, index) => (
-          <button  key={index}>{game.gameId}</button>
-        ))}
-      </ul>
+   
+     
     </>
   );
 }
